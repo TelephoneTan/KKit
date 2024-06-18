7cb9639158ce0496b6805b3e5f0d325e18784a21
Telephone <telephone.tan@qq.com>
2024-06-18 19:54:09 +0800

    修复 onWasmReady 缺失导致概率性崩溃的问题

--- composeApp/src/jsMain/kotlin/Main.kt
+++ composeApp/src/jsMain/kotlin/Main.kt
@@ -1,13 +1,16 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
     console.log(Greeting().greet())
     if (isBrowser) {
-        @OptIn(ExperimentalComposeUiApi::class)
-        ComposeViewport(document.body!!) {
-            App()
+        onWasmReady {
+            @OptIn(ExperimentalComposeUiApi::class)
+            ComposeViewport(document.body!!) {
+                App()
+            }
         }
     }
 }
