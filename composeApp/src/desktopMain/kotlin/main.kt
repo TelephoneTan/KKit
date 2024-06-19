a6eaac02f9af61399fc5b9ca92f55c262d5a6e50
Telephone <telephone.tan@qq.com>
2024-06-20 06:41:25 +0800

    将 Zipline 相关的逻辑集中到 ziplineMain 中

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,4 +1,3 @@
-import KKit.shared.BuildConfig
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import kotlinx.coroutines.runBlocking
@@ -6,11 +5,12 @@ import kotlinx.coroutines.runBlocking
 fun main() = runBlocking {
     js().run {
         hello()
+        val latestZ = latestZiplineVersion()
         application {
             Window(
                 onCloseRequest = ::exitApplication,
                 title = "${world()}[" +
-                        "current:${version()}, latest:${BuildConfig.ZIPLINE_JS_VERSION}" +
+                        "current:${version()}, latest:$latestZ" +
                         "]" +
                         "@${Hosts.SERVER}" +
                         ":${Ports.SERVER}",
--- shared/src/ziplineMain/kotlin/Utils.kt
+++ shared/src/ziplineMain/kotlin/Utils.kt
@@ -1,1 +1,4 @@
+import KKit.shared.BuildConfig
+
+suspend fun latestZiplineVersion(): Int = BuildConfig.ZIPLINE_JS_VERSION
 internal fun isZiplineJSFresh(cachedVersion: String?) = false
